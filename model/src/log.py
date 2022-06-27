13966a5ec9608bcd7a0f88d0fb427c80bc5624fa
JacobHuang <9145821+binghamhuang@user.noreply.gitee.com>
2022-06-27 08:07:18 +0000

    update model/src/log.py.

--- model/src/log.py
+++ model/src/log.py
@@ -9,22 +9,22 @@ import sys
 import os
 
 # msadvisor提供的打印公共接口
-def ad_print(log_level, log_info):
+def print(log_level, log_info):
     file_name = os.path.basename(os.path.realpath(__file__))
     file_line = str(sys._getframe(1).f_lineno)
     full_log_info = '[' + file_name + ':' +file_line + '] ' + log_info
-    m.log.PrintLog(log_level, full_log_info)
+    m.utils.print(log_level, full_log_info)
 
 # msadvisor提供的打印并记录日志公共接口
-def ad_print_and_log(log_level, log_info):
+def print_and_log(log_level, log_info):
     file_name = os.path.basename(os.path.realpath(__file__))
     file_line = str(sys._getframe(1).f_lineno)
     full_log_info = '[' + file_name + ':' +file_line + '] ' + log_info
-    m.log.PrintAndLog(log_level, full_log_info)
+    m.utils.print_and_log(log_level, full_log_info)
 
 # msadvisor提供的记录日志公共接口
-def ad_log(log_level, log_info):
+def log(log_level, log_info):
     file_name = os.path.basename(os.path.realpath(__file__))
     file_line = str(sys._getframe(1).f_lineno)
     full_log_info = '[' + file_name + ':' +file_line + '] ' + log_info
-    m.log.AdLog(log_level, full_log_info)
+    m.utils.log(log_level, full_log_info)
