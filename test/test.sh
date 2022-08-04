38a13aa2999193715f290fb38583d6c933c642ce
block0219 <chenweihua5@huawei.com>
2022-08-04 10:28:19 +0800

    add coverage

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-pip3 install coverage
+set -u
 
-cp ../auto_optimizer ./ -rf
+pwd_dir=${PWD}
+
+# copy auto_optimizer to test file, and test
+cp ${pwd_dir}/../auto_optimizer ${pwd_dir}/ -rf
 
 coverage run -p -m unittest
 coverage combine
-coverage report -m
+coverage report -m --omit="test_*.py" > ${pwd_dir}/test.coverage
+
+coverage_line=`cat ${pwd_dir}/test.coverage | grep "TOTAL" | awk '{print $4}' | awk '{print int($0)}'`
+
+echo "coverage_line="$coverage_line
+
+if [ ${coverage_line} -lt 60 ]; then
+    echo "coverage failed"
+    exit -1
+fi
