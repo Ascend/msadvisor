1c51803c53a9fd820ea5f24b9a45665a28cf18b3
dingli <dingli40@huawei.com>
2022-08-21 17:01:30 -0900

    fix for reusability

--- auto_optimizer/graph_refactor/interface/base_graph.py
+++ auto_optimizer/graph_refactor/interface/base_graph.py
@@ -12,60 +12,155 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from itertools import chain
 from abc import ABC, abstractmethod
+from typing import List, Dict, Union
+
+from auto_optimizer.graph_refactor.onnx.node import PlaceHolder, Initializer, Node
 
 class BaseGraph(ABC):
+
+    def __init__(
+        self,
+        nodes: List[Node] = None,
+        inputs: List[PlaceHolder] = None,
+        outputs: List[PlaceHolder] = None,
+        initializers: List[Initializer] = None,
+        value_infos: List[PlaceHolder] = None,
+        name: str = None,
+        **kwargs: Dict[str, object]
+    ):
+        self._nodes = nodes if nodes else []
+        self._inputs = inputs if inputs else []
+        self._outputs = outputs if outputs else []
+        self._initializers = initializers if initializers else []
+        self._value_infos = value_infos if value_infos else []
+        self.name = name
+
+        self._node_map = {}
+        self._prev_map = {}
+        self._next_map = {}
+
+        self._meta = {
+                    'ir_version': kwargs.get('ir_version', 4),
+                    'producer_name': kwargs.get('producer_name', 'AutoOptimizer'),
+                    'producer_version': kwargs.get('producer_version', 'alpha'),
+                    'domain': kwargs.get('domain', ''),
+                    'model_version': kwargs.get('model_version', 0),
+                    'opset_imports': kwargs.get('opset_imports', None)
+        }
+
+        self.update_map()
+    
+    def update_map(self):
+        # clear map first
+        self._node_map = {}
+        self._prev_map = {}
+        self._next_map = {}
+
+        self._node_map = {n.name: n for n in 
+                        chain(self._inputs, self._outputs, self._nodes, self._initializers, self._value_infos)}
+        # update prev node info
+        for n in self._nodes:
+            for o in n.outputs:
+                # if output name not in map
+                if not self._prev_map.get(o):
+                    self._prev_map[o] = n
+                else:
+                    # TODO: ERROR: duplicate output names
+                    pass
+        # update next node info
+        for n in self._nodes:
+            for i in n.inputs:
+                if not self._next_map.get(i):
+                    self._next_map[i] = [n]
+                else:
+                    self._next_map[i].append(n)
 
     @classmethod
     @abstractmethod
     def parse(cls, model):
         pass
 
-    @abstractmethod
     def add_input(self, name, dtype, shape):
         pass
 
-    @abstractmethod
     def add_output(self, name, dtype, shape):
         pass
 
-    @abstractmethod
     def add_initializer(self, name, value):
         pass
 
-    @abstractmethod
     def add_node(self, name, op_type, attrs=None, domain=None):
         pass
 
-    @abstractmethod
     def insert_node(self, refer_name, insert_node, refer_io_index=0, mode='after'):
         pass
 
-    @abstractmethod
     def get_nodes(self, op_type):
         pass
 
-    @abstractmethod
     def remove(self, name, maps=None):
         pass
 
-    @abstractmethod
     def __getitem__(self, key):
         pass
 
-    @abstractmethod
     def __setitem__(self, key, value):
         pass
 
     @property
-    @abstractmethod
-    def inputs(self):
-        pass
+    def inputs(self) -> List[PlaceHolder]:
+        return self._inputs
 
     @property
-    @abstractmethod
-    def outputs(self):
-        pass
+    def outputs(self) -> List[PlaceHolder]:
+        return self._outputs
+
+    @property
+    def nodes(self) -> List[Node]:
+        return self._nodes
+
+    @property
+    def initializers(self) -> List[Initializer]:
+        return self._initializers
+
+    def get_prev_node(self, input_name: str) -> Union[Node, PlaceHolder, Initializer]:
+        # TODO: raise exception
+        return self._prev_map.get(input_name, None)
+
+    def get_next_nodes(self, output_name: str) -> Union[List[Node], List[PlaceHolder], List[Initializer]]:
+        # TODO: raise exception
+        return self._next_map.get(output_name, [])
+
+    def toposort(self):
+        def visited_all_prev_nodes(node, visited):
+            for input_name in node.inputs:
+                prev_node = self.get_prev_node(input_name)
+                if prev_node not in visited and prev_node:
+                    return False
+            return True
+
+        queue = deque()
+        visited = set()
+        for node in self._nodes:
+            if visited_all_prev_nodes(node, visited):
+                queue.append(node)
+        
+        sorted_nodes = []
+        while queue:
+            node = queue.popleft()
+            if visited_all_prev_nodes(node, visited):
+                sorted_nodes.append(node)
+                visited.add(node)
+                for output_name in node.outputs:
+                    for next_node in self.get_next_nodes(output_name):
+                        if next_node not in queue and next_node not in visited:
+                            queue.append(next_node)
+            else:
+                queue.append(node)
+        
+        self._nodes = sorted_nodes
 
     @abstractmethod
     def save(self, path):
--- auto_optimizer/graph_refactor/onnx/graph.py
+++ auto_optimizer/graph_refactor/onnx/graph.py
@@ -25,6 +25,7 @@ from auto_optimizer.graph_refactor.interface.base_graph import BaseGraph
 from auto_optimizer.graph_refactor.onnx.node import PlaceHolder, Initializer, Node
 
 class OnnxGraph(BaseGraph):
+
     def __init__(
         self,
         nodes: List[Node] = None,
@@ -35,28 +36,7 @@ class OnnxGraph(BaseGraph):
         name: str = None,
         **kwargs: Dict[str, object]
     ):
-        super().__init__()
-        self._nodes = nodes if nodes else []
-        self._inputs = inputs if inputs else []
-        self._outputs = outputs if outputs else []
-        self._initializers = initializers if initializers else []
-        self._value_infos = value_infos if value_infos else []
-        self.name = name
-
-        self._node_map = {}
-        self._prev_map = {}
-        self._next_map = {}
-
-        self._meta = {
-                    'ir_version': kwargs.get('ir_version', 4),
-                    'producer_name': kwargs.get('producer_name', 'AutoOptimizer'),
-                    'producer_version': kwargs.get('producer_version', 'alpha'),
-                    'domain': kwargs.get('domain', ''),
-                    'model_version': kwargs.get('model_version', 0),
-                    'opset_imports': kwargs.get('opset_imports', None)
-        }
-
-        self.update_map()
+        super(OnnxGraph, self).__init__(nodes, inputs, outputs, initializers, value_infos, name, **kwargs)
 
     @classmethod
     def parse(cls, path_or_bytes: Union[str, ModelProto, GraphProto]) -> 'OnnxGraph':
@@ -89,31 +69,6 @@ class OnnxGraph(BaseGraph):
 
         graph = cls(nodes, inputs, outputs, initializers, value_infos, onnx_graph.name, **meta)
         return graph
-
-    def update_map(self):
-        # clear map first
-        self._node_map = {}
-        self._prev_map = {}
-        self._next_map = {}
-
-        self._node_map = {n.name: n for n in 
-                        chain(self._inputs, self._outputs, self._nodes, self._initializers, self._value_infos)}
-        # update prev node info
-        for n in self._nodes:
-            for o in n.outputs:
-                # if output name not in map
-                if not self._prev_map.get(o):
-                    self._prev_map[o] = n
-                else:
-                    # TODO: ERROR: duplicate output names
-                    pass
-        # update next node info
-        for n in self._nodes:
-            for i in n.inputs:
-                if not self._next_map.get(i):
-                    self._next_map[i] = [n]
-                else:
-                    self._next_map[i].append(n)
 
     def add_placeholder(self, name, dtype, shape, ph_type='input'):
         pass
