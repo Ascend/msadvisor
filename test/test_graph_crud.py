ff22f69777ccc40df2b94bc5323212688474e721
Dingli <dingli40@huawei.com>
2022-08-28 16:46:55 -0900

    fix graph remove & add ut

--- test/test_graph_crud.py
+++ test/test_graph_crud.py
@@ -25,14 +25,15 @@ from test_graph_basic import is_graph_equal
 def create_graph():
     input_0 = OnnxPlaceHolder('input_0', np.dtype('float32'), [1,3,224,224])
     output_0 = OnnxPlaceHolder('output_0', np.dtype('float32'), [1,3,224,224])
+    ini_0 = OnnxInitializer('ini_0', np.array([1,2,3], dtype='float32'))
     node_0 = OnnxNode('Node_0', 'Sub', inputs=['input_0'], outputs=['0_out_0', '0_out_1'], attrs={})
     node_1 = OnnxNode('Node_1', 'Mul', inputs=['0_out_0'], outputs=['1_out_0'], attrs={})
     node_2 = OnnxNode('Node_2', 'Add', inputs=['0_out_0', '0_out_1'], outputs=['2_out_0', '2_out_1'], attrs={})
-    node_3 = OnnxNode('Node_3', 'Sub', inputs=['1_out_0'], outputs=['3_out_0'], attrs={})
+    node_3 = OnnxNode('Node_3', 'Sub', inputs=['1_out_0', 'ini_0'], outputs=['3_out_0'], attrs={})
     node_4 = OnnxNode('Node_4', 'Add', inputs=['1_out_0', '2_out_0'], outputs=['4_out_0'], attrs={})
     node_5 = OnnxNode('Node_5', 'Mul', inputs=['3_out_0', '4_out_0', '2_out_1'], outputs=['output_0'], attrs={})
-    return OnnxGraph([node_0,node_1,node_2,node_3,node_4,node_5], [input_0], [output_0])
-    
+    return OnnxGraph([node_0,node_1,node_2,node_3,node_4,node_5], [input_0], [output_0], [ini_0])
+
 class TestGraphCrud(unittest.TestCase):
 
     def setUp(self):
@@ -41,6 +42,11 @@ class TestGraphCrud(unittest.TestCase):
         self.addTypeEqualityFunc(OnnxInitializer, is_ini_equal)
         self.addTypeEqualityFunc(OnnxGraph, is_graph_equal)
         self.graph = create_graph()
+
+    # def test_graph_test(self):
+    #     target = OnnxGraph()
+    #     self.assertEqual(self.graph, target)
+    #     # self.assertTrue(is_graph_equal(self.graph, target))
 
     def test_add_input(self):
         test_input = self.graph.add_input('test_input', 'float32', [1,3,224,224])
@@ -55,7 +61,7 @@ class TestGraphCrud(unittest.TestCase):
     def test_add_initializer(self):
         test_ini = self.graph.add_initializer('test_ini', np.array([1,2,3]))
         self.assertEqual(self.graph['test_ini'], test_ini)
-        self.assertEqual(self.graph.initializers, [test_ini])
+        self.assertEqual(self.graph.initializers, [self.graph['ini_0'], test_ini])
 
     def test_add_node(self):
         test_node = self.graph.add_node('test_node', 'Add')
@@ -103,6 +109,21 @@ class TestGraphCrud(unittest.TestCase):
         # create target
         input_0 = OnnxPlaceHolder('input_0', np.dtype('float32'), [1,3,224,224])
         output_0 = OnnxPlaceHolder('output_0', np.dtype('float32'), [1,3,224,224])
+        ini_0 = OnnxInitializer('ini_0', np.array([1,2,3], dtype='float32'))
+        node_0 = OnnxNode('Node_0', 'Sub', inputs=['input_0'], outputs=['0_out_0', '0_out_1'], attrs={})
+        node_1 = OnnxNode('Node_1', 'Mul', inputs=['0_out_0'], outputs=['1_out_0'], attrs={})
+        node_2 = OnnxNode('Node_2', 'Add', inputs=['0_out_0', '0_out_1'], outputs=['2_out_0', '2_out_1'], attrs={})
+        node_4 = OnnxNode('Node_4', 'Add', inputs=['1_out_0', '2_out_0'], outputs=['4_out_0'], attrs={})
+        node_5 = OnnxNode('Node_5', 'Mul', inputs=['1_out_0', '4_out_0', '2_out_1'], outputs=['output_0'], attrs={})
+        target = OnnxGraph([node_0,node_1,node_2,node_4,node_5], [input_0], [output_0], [ini_0])
+
+        self.graph.remove('Node_3')
+        self.assertEqual(self.graph, target)
+
+    def test_graph_remove_ini_node(self):
+        # create target
+        input_0 = OnnxPlaceHolder('input_0', np.dtype('float32'), [1,3,224,224])
+        output_0 = OnnxPlaceHolder('output_0', np.dtype('float32'), [1,3,224,224])
         node_0 = OnnxNode('Node_0', 'Sub', inputs=['input_0'], outputs=['0_out_0', '0_out_1'], attrs={})
         node_1 = OnnxNode('Node_1', 'Mul', inputs=['0_out_0'], outputs=['1_out_0'], attrs={})
         node_2 = OnnxNode('Node_2', 'Add', inputs=['0_out_0', '0_out_1'], outputs=['2_out_0', '2_out_1'], attrs={})
@@ -110,6 +131,7 @@ class TestGraphCrud(unittest.TestCase):
         node_5 = OnnxNode('Node_5', 'Mul', inputs=['1_out_0', '4_out_0', '2_out_1'], outputs=['output_0'], attrs={})
         target = OnnxGraph([node_0,node_1,node_2,node_4,node_5], [input_0], [output_0])
 
+        self.graph.remove('ini_0')
         self.graph.remove('Node_3')
         self.assertEqual(self.graph, target)
 
