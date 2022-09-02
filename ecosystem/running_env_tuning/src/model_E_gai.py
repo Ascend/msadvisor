3f37c752ab8d04a7fc7c81144d108e5941ee48a0
user <2289112474@qq.com>
2022-09-02 08:46:11 +0800

    调试E 增加user_parameter变量

--- ecosystem/running_env_tuning/src/model_E_gai.py
+++ ecosystem/running_env_tuning/src/model_E_gai.py
@@ -71,7 +71,7 @@ def Evaluate(datapath, parameter):
 
     environment_data = get_data(environment_filename, datapath, target_path)  # 获取系统配置文件的数据environmentConfig.json
     # 获取用户配置文件的数据ecosystem.json   第二个参数是服务器上对应文件工程的地址/home/zjut-msadvisor/group1/yyh/msadvisor_2/ecosystem，本地调试的话使用../../
-    user_data = get_data(user_filename, '../../', "running_env_tuning")
+    user_data = get_data(user_filename, '/home/zjut-msadvisor/group1/yyh/msadvisor_2/ecosystem', "running_env_tuning")
     user_parameter = user_data.get('model_list')[0].get("session_list")[0].get("parameter")
 
     print(user_parameter)
