96f9ecbe1c13978aec90fef7111bd77418f23fce
Dingli <dingli40@huawei.com>
2022-09-22 23:31:34 -0900

    fix om compiler

--- auto_optimizer/inference_engine/model_convert/om_compiler.py
+++ auto_optimizer/inference_engine/model_convert/om_compiler.py
@@ -19,18 +19,28 @@ from .compiler import Compiler
 
 class OmCompiler(Compiler):
 
+    @staticmethod
+    def _check_required_params(cfg):
+        required_params = ('type', 'framework', 'model', 'output', 'soc_version')
+        params = cfg.keys()
+        for param in required_params:
+            if param not in params:
+                raise RuntimeError("Parameter missing! '{}' is required in om convert!".format(param))
+
     def __init__(self, cfg):
-        self.atc_cmd = [
-                'atc',
-                '--framework={}'.format(cfg['framework']),
-                '--model={}'.format(cfg['model']),
-                '--output={}'.format(cfg['output']),
-                '--input_shape={}'.format(cfg['input_shape']),
-                '--soc_version={}'.format(cfg['soc_version']),
-        ]
-        if cfg.get('custom_cfg', None):
-            for key, value in self.cfg['custom_cfg'].items:
-                self.atc_cmd.append('--{}={}'.format(key, value))
+        OmCompiler._check_required_params(cfg)
+        cmd_type = cfg['type']
+        self.atc_cmd = []
+        if cmd_type == 'atc':
+            self.atc_cmd.append('atc')
+            for key, value in cfg.items():
+                if key != 'type':
+                    self.atc_cmd.append('--{}={}'.format(key, value))
+        elif cmd_type == 'aoe':
+            self.atc_cmd.append('aoe')
+        else:
+            raise RuntimeError("Invalid cmd type! Only support 'atc', 'aoe', but got '{}'.".format(cmd_type))
 
     def build_model(self):
+        print(self.atc_cmd)
         subprocess.run(self.atc_cmd, shell=False)
