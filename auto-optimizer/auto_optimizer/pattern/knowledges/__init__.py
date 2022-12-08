e08fb144d56774922a127ea309f7a2d69e791216
JacobHuang <huangjianbin1@huawei.com>
2022-12-09 03:35:03 +0800

    gather_to_split

--- auto-optimizer/auto_optimizer/pattern/knowledges/__init__.py
+++ auto-optimizer/auto_optimizer/pattern/knowledges/__init__.py
@@ -8,3 +8,4 @@ from .knowledge_split_large_kernel import KnowledgeSplitLargeKernelConv
 from .knowledge_resize_mode_to_nearest import KnowledgeResizeModeToNearest
 from .knowledge_topk_fix import KnowledgeTopkFix
 from .knowledge_merge_casts import KnowledgeMergeCasts
+from .knowledge_gather_to_split import KnowledgeGatherToSplit
