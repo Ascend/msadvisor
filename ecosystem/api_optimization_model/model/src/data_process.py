41f4b440f8b8d98a9767698ad9cf373d6adc4a53
binghamhuang <huangjianbin1@huawei.com>
2023-02-13 21:33:50 +0800

    commit 2.13 21:33

--- ecosystem/api_optimization_model/model/src/data_process.py
+++ ecosystem/api_optimization_model/model/src/data_process.py
@@ -4,7 +4,14 @@ import os
 knowledges = {
     "aclopCreateKernel": "检查aclopCreateKernel接口是否用到了aclEngineType枚举，如果使用请修改代码并重新编译",
     "aclmdlGetOutputNameByIndex": "代码使用了aclmdlGetOutputNameByIndex接口，请重新进行atc模型转换，如果模型包含top名称则还需要适配返回值并重新编译",
-    "aclrtFreeHost": "代码使用了aclrtFreeHost，但未使用aclrtCreateContext或aclrtSetCurrentContext接口显示设置context，请增加代码适配，并重新编译"
+    "aclrtFreeHost": "代码使用了aclrtFreeHost，但未使用aclrtCreateContext或aclrtSetCurrentContext接口显示设置context，请增加代码适配，并重新编译",
+    "aclgrphBuildModel": "",
+    "aclcreateEvent": "检查aclcreateEvent接口创建的event资源是否超过了1023，若超过了会创建失败",
+    "aclmdlGetOutputNameByIndex": "代码使用了aclmdlGetOutputNameByIndex接口，需要重新进行atc模型转换，如果模型包含top名称还需要适配返回值并重新编译",
+    "aclgrphParseCaffe": "",
+    "aclgrphParseTensorFlow": "",
+    "aclgrphParseONNX": "",
+    "aclgrphParseONNXFromMem": "",
 }
 
 
