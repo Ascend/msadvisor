c33c5b277fcb5ad1464cb39efad7c4b3eaaa0b33
binghamhuang <huangjianbin1@huawei.com>
2023-02-15 21:26:21 +0800

    commit 2.15 21:26

--- ecosystem/api_optimization_model/model/src/data_process.py
+++ ecosystem/api_optimization_model/model/src/data_process.py
@@ -10,11 +10,11 @@ sys.path.append(os.path.dirname(__file__))
 from util import log
 
 def data_process(file_pathname, extend_result):
-    # 遍历该目录下的所有code文件
     if not os.path.isdir(file_pathname):
         log.ad_log(log.AD_ERROR, "The file_pathname is incorrect. Please check file_pathname path.")
         return extend_result
     log.ad_log(log.ad_info, "Start scanning file.")
+    # 遍历该目录下的所有code文件
     for root, dirs, files in os.walk(file_pathname):
         for file in files:
             if file.endswith('.cpp') or file.endswith('.py') or file.endswith('.h'):
